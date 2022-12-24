61e681772a446fd55ac0a90e59cc8aeb9d1fe6b2
Akash Pandit <34327539+FishInABucket@users.noreply.github.com>
2022-12-23 17:10:08 -0800

    Update Attacks.py
    added a couple new attacks and noted down bug with status effects

--- Attacks.py
+++ Attacks.py
@@ -1,7 +1,7 @@
 import random
 
 
-def chance_of_activation_25(status):
+def chance_25(status):
     if random.randint(1, 4) == 1:
         return status
     else:
@@ -10,15 +10,16 @@ def chance_of_activation_25(status):
 
 class Attacks:
     def __init__(self, name, type_, dmg, desc, effect=None):
-        self.name = name
+        self.name = name.capitalize()
         self.type = type_
         self.dmg = dmg
         self.desc = desc
         self.effect = effect
 
     def __str__(self):
-        return f"{self.name.upper()} \n\tMove Typing: {self.type} \n\tDamage: {self.dmg} \n\tDescription: {self.desc}\n"
+        return f"{self.name} \n\tMove Typing: {self.type} \n\tDamage: {self.dmg} \n\tDescription: {self.desc}\n"
 
+        # FIXME: status effects not applying effects for longer than a single move
     def effect_activate(self, opp):
         if self.effect.lower() == 'paralysis':
             if random.randint(1, 4) == 1:
@@ -47,10 +48,22 @@ class Attacks:
 tackle = Attacks('tackle', 'Normal', 3, "The user charges against the opponent, hitting them with the full force of "
                                         "their weight.")
 gooSpray = Attacks('goo spray', 'Poison', 2, "The user spits a wad of goo on the opponent with a 25% chance to"
-                                             "inflict poison on them.", chance_of_activation_25('poison'))
-infernoSausage = Attacks('inferno sausage', 'Fire', 8,
-                         "The hot pocket opens itself up, spilling its contents"
-                         "on you. 25% chance to be burned.", chance_of_activation_25('burn'))
-firestarter = Attacks('fire starter', 'Fire', 0, "The user lights the opponent on fire, causing burn status.", 'burn')
+                                             " inflict poison on them.", chance_25('poison'))
+flamingSausage = Attacks('Flaming Sausage', 'Fire', 8, "The hot pocket opens itself up, spilling its contents\n"
+                                                       "on you. 25% chance to be burned.", chance_25('burn'))
+fireStarter = Attacks('fire starter', 'Fire', 0, "The user lights the opponent on fire, causing burn status.", 'burn')
 
 wait = Attacks('wait', 'Normal', 0, "The user does nothing, just sits and waits.")
+
+trauma = Attacks('traumatize', 'Psychic', 5, 'The user trauma dumps all their pain upon the opponent, leaving them\n'
+                                             'with psychological scarring and requiring therapy for the rest of their\n'
+                                             'life.')
+digitsPi = Attacks("1 million digits of pi", 'Psychic', 2, 'The user recites 1 million digits of pi at such a fast \n'
+                                                           'rate that the opponent loses half their braincells.')
+
+woodenSpoon = Attacks('Wooden Spoon', 'Fighting', 6, 'The user slams the opponent upside the head with a wooden spoon.')
+
+comparison = Attacks('Comparing to your Cousin', 'Psychic', 3, 'The user compares the opponent to their cousin who \n'
+                                                               'always seems to do everything right while they always\n'
+                                                               'seem to do everything wrong.')
+
