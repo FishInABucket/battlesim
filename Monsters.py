bcbfd8bcca697a55acdfe710f2e94c664dcbf558
Akash Pandit <34327539+FishInABucket@users.noreply.github.com>
2022-12-23 17:16:18 -0800

    Update Monsters.py
    moved battle execution to main.py, added new monsters, cleaned up monster class a little bit and added comments for readability

--- Monsters.py
+++ Monsters.py
@@ -2,18 +2,21 @@ import Attacks as a
 
 monsters = {
     # species: (max_hp, move1, move2, status_effect)
-    "slime": (10, a.tackle, a.gooSpray, None),
-    "hockypocky": (20, a.firestarter, a.infernoSausage, None),
-    "test dummy": (1000, a.wait, a.wait, None)
+    "slime": ('Slime', 10, a.tackle, a.gooSpray, None),
+    "hotpocket": ('Sentient Hot Pocket', 20, a.fireStarter, a.flamingSausage, None),
+    "tank": ('Tank Test Dummy', 1000, a.wait, a.wait, None),
+    "glass": ('Glassy Dummy', 1, a.wait, a.wait, None),
+    "stem student": ('STEM Student', 69, a.trauma, a.digitsPi, None),
+    "asian mom": ('Asian Mom', 80, a.woodenSpoon, a.comparison, None),
 }
 
 
 class Monster:
     def __init__(self, name):
-        self.name = name
-        self.max_hp, self.current_hp = monsters[name][0], monsters[name][0]
-        self.move1, self.move2 = monsters[name][1], monsters[name][2]
-        self.status = monsters[name][3]
+        self.name = monsters[name][0]
+        self.max_hp, self.current_hp = monsters[name][1], monsters[name][1]
+        self.move1, self.move2 = monsters[name][2], monsters[name][3]
+        self.status = monsters[name][4]
         self.exp, self.total_exp = 0, 0
         self.level = 1
         self.wins, self.losses = 0, 0
@@ -25,15 +28,25 @@ class Monster:
         print(f'{self.name} wins! \n\tTotal wins: {self.wins}\n\tEXP gained: +5\n\t')
 
     def lose_fight(self):
+        """resets hp and adds a loss to the counter"""
         self.current_hp = self.max_hp
         self.losses += 1
 
     def update_exp(self):
+        """initializes the exp needed to level up your mon, then depending on what level you're at (1-9, 10-19, 20-29,
+        etc.), it provides a multiplier to how much exp is needed.
+        EX: Lv. 1-9 is 10 exp (base)
+        Lv. 10-19 is 15 exp (*1.5)
+        Lv. 20-29 is 30 exp (*2)
+        Lv. 30-39 is 45 exp (*1.5) etc."""
         level_up_exp = 10
         if (self.level // 10) % 2 == 0 and self.level // 10 > 0:
             level_up_exp *= 1.5
         elif (self.level // 10) % 2 != 0 and self.level // 10 > 0:
             level_up_exp *= 2
+
+        """logs how many times a monster would level up with its current exp, tells the user, and levels them up those
+        many times. Every 5 levels, damaging moves gain +1 dmg, and every level health gets raised by 1."""
         levelups = self.exp // level_up_exp
         if levelups > 0:
             print(f'LEVELED UP! {self.level} -> {self.level + levelups}\n\tHP: +1')
@@ -51,6 +64,7 @@ class Monster:
         print(f'EXP to level up: {level_up_exp - self.exp}')
 
     def print_stats(self):
+        """Prints the relevant stats about your monster object. Currently unused outside of testing."""
         print('#' * 50)
         print(f'Monster: {self.name}, HP: {self.current_hp}/{self.max_hp}')
         print(f'Level: {self.level}, EXP to level up: {self.exp}, Total EXP: {self.total_exp}')
@@ -60,6 +74,7 @@ class Monster:
         print('#' * 50)
 
     def attack_menu(self):
+        """A condensed version of .print_stats() used when battling."""
         print('#' * 50)
         print(f'{self.name}, HP: {self.current_hp}/{self.max_hp}')
         print(f'Moves:\n{self.move1}\n{self.move2}')
@@ -80,13 +95,12 @@ class Monster:
 
         self.attack_menu()
         atk = input("Pick an attack: ")
-        if atk == self.move2.name:
-            print(True)
-        while atk != self.move1.name and atk != self.move2.name:
+        while atk.lower().capitalize() != self.move1.name and atk.lower().capitalize() != self.move2.name:
             atk = input(f"That is not a valid attack. Here are all valid attacks for this monster: "
-                        f"{self.move1.name}, {self.move2.name}")
+                        f"{self.move1.name}, {self.move2.name}\n")
 
         def _atk_output(move):
+            """Created to minimize repetitive code, executes various text messages based on the attack used."""
             if move.dmg > 0:
                 other.current_hp -= move.dmg
                 print(f'{self.name} hit {other.name} with {move.name} for {move.dmg} damage!')
@@ -100,63 +114,8 @@ class Monster:
             else:
                 'Error 2: something unexpected happened'
 
-        if atk == self.move1.name:
+        # Checks if an attack input matches either move 1 or move 2 for that mon, then executes the move if it does.
+        if atk.lower().capitalize() == self.move1.name:
             _atk_output(self.move1)
-        elif atk == self.move2.name:
+        elif atk.lower().capitalize() == self.move2.name:
             _atk_output(self.move2)
-
-
-if __name__ == '__main__':
-    print(f'-' * 20, 'Pick your monsters', '-' * 20)
-    """Initialize monster variables"""
-    p1 = ''
-    p2 = ''
-
-    """
-    Monster Selection
-    """
-    while p1 not in monsters:
-        p1 = input('Pick your first monster: ')
-        p1 = p1.lower()
-        if p1.lower() not in monsters:
-            print('Please pick an existing monster.')
-            print(f'List of current monsters:', end=' ')
-            for i in range(len(monsters)):
-                if tuple(monsters.keys())[i] is not tuple(monsters.keys())[-1]:
-                    print(tuple(monsters.keys())[i], end=', ')
-                else:
-                    print(tuple(monsters.keys())[-1])
-    p1 = Monster(p1)
-
-    while p2 not in monsters:
-        p2 = input('Pick your second monster: ')
-        p2 = p2.lower()
-        if p2.lower() not in monsters:
-            print('Please pick an existing monster.')
-            print(f'List of current monsters:', end=' ')
-            for i in range(len(monsters)):
-                if tuple(monsters.keys())[i] is not tuple(monsters.keys())[-1]:
-                    print(tuple(monsters.keys())[i], end=', ')
-                else:
-                    print(tuple(monsters.keys())[-1])
-    p2 = Monster(p2)
-
-    """
-    battling
-    """
-    print(f'-' * 20, 'BATTLE BEGIN', '-' * 20)
-    round_num = 1
-    while True:
-        if p1.fight(p2) == p1.name:
-            p1.win_fight()
-            p2.lose_fight()
-            p1.update_exp()
-            break
-        round_num += 1
-        if p2.fight(p1) == p2.name:
-            p2.win_fight()
-            p1.lose_fight()
-            p2.update_exp()
-            break
-        round_num += 1
-
